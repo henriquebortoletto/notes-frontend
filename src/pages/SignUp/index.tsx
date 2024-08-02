141cc2785722e4f259aa5add6e3d41cc0106b7ac
Henrique Bortoletto <bortolettohenrique@gmail.com>
2024-08-02 18:15:30 -0300

    feat: funcionalidade de cadastrar usuario

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,28 +1,77 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
+
+import { createUserService } from "@/services/userService";
 
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 
 import * as S from "./styles";
 
-const SignUp = () => (
-  <S.Wrapper>
-    <S.Background />
-    <S.Box>
-      <S.Title>Rocket Notes</S.Title>
-      <S.Description>
-        Aplicação para salvar e gerenciar seus links úteis.
-      </S.Description>
-      <S.Login>Crie sua conta</S.Login>
-      <S.Form>
-        <Input $icon={FiUser} type="text" placeholder="Nome" />
-        <Input $icon={FiMail} type="email" placeholder="E-mail" />
-        <Input $icon={FiLock} type="password" placeholder="Senha" />
-        <Button $title="Cadastrar" type="submit" />
-      </S.Form>
-      <S.SignInLink to="/">Voltar para o login</S.SignInLink>
-    </S.Box>
-  </S.Wrapper>
-);
+const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!name || !email || !password) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    createUserService({ name, email, password })
+      .then(() => {
+        alert("Usuário criado com sucesso");
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.data.message) {
+          alert(error.response.data.message);
+        } else {
+          alert("Ocorreu um erro inesperado");
+        }
+      });
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Background />
+      <S.Box>
+        <S.Title>Rocket Notes</S.Title>
+        <S.Description>
+          Aplicação para salvar e gerenciar seus links úteis.
+        </S.Description>
+        <S.Login>Crie sua conta</S.Login>
+        <S.Form onSubmit={handleFormSubmit}>
+          <Input
+            $icon={FiUser}
+            type="text"
+            placeholder="Nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            $icon={FiMail}
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            $icon={FiLock}
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button $title="Cadastrar" type="submit" />
+        </S.Form>
+        <S.SignInLink to="/">Voltar para o login</S.SignInLink>
+      </S.Box>
+    </S.Wrapper>
+  );
+};
 
 export default SignUp;
